1ebbfdaef92aa88eb9016a4e45144fd137313aff
Ayush-bhoyar <ayush2bhoyar@gmail.com>
2025-03-15 20:14:10 +0530

    created the groovy script for build and docker push

--- vars/buildAndPushDocker.groovy
+++ vars/buildAndPushDocker.groovy
@@ -1,5 +1,5 @@
 def call(String imageName, String dockerCredId) {
-    withCredentials([usernamePassword(credentialsId: dockerCredId, usernameVariable: 'DOCKER_USER', passwordVariable: 'DOCKER_PASS')]) {
+    withCredentials([usernamePassword(credentialsId: dockerhub-cred, usernameVariable: 'DOCKER_USER', passwordVariable: 'DOCKER_PASS')]) {
         sh 'docker login -u "$DOCKER_USER" -p "$DOCKER_PASS"'
     }
     sh "docker build -t ${imageName} ."
